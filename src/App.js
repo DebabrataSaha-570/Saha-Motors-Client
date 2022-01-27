400b8afd2bf6053181723b9efc8d1ac7facfc1a6
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-01-27 22:14:16 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div >
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="*">
+          <NotFound></NotFound>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Page Not Found!!</h2>
+            <h3>404, Error</h3>
+        </div>
+    );
+};
+
+export default NotFound;
